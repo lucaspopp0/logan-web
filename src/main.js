83264b18f57ec7542ccf2f33833d8aee96fabd31
Lucas Popp <lmp122@case.edu>
2020-02-10 15:34:14 +0100

    Finished adding google sign-in button

--- src/main.js
+++ src/main.js
@@ -6,6 +6,21 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.component('g-signin-button', {
+  template: '<div class="g-signin2" ref="gLoginButton"></div>',
+  mounted () {
+    window.gapi.load('auth2', () => {
+      const auth2 = window.gapi.auth2.init({
+        client_id: '261132618985-m4eb9huuqosfddibg17jgm50d69g0ki3.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin'
+      })
+      auth2.attachClickHandler(this.$refs.gLoginButton, {}, googleUser => {
+        this.$emit('done', googleUser)
+      }, error => console.log(error))
+    })
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
