4d4489b83333bf5beeba9f32e4cbb363bd117154
Lucas Popp <lmp122@case.edu>
2020-02-10 15:33:40 +0100

    Added sign-in button to page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <header>
-      <span>Vue.js PWA</span>
+      <g-signin-button></g-signin-button>
     </header>
     <main>
       <img src="./assets/logo.png" alt="Vue.js PWA">
@@ -35,20 +35,8 @@ main {
 
 header {
   margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
+  padding: 0.5em 16px;
   background-color: #35495E;
   color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
 }
 </style>
