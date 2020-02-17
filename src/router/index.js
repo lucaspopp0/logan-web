5fd04b4d944ecafa7979e3f0d12a72ab61854707
Lucas Popp <lmp122@case.edu>
2020-02-17 17:18:47 +0100

    Fixed references

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-import Tasks from '@/components/Tasks'
-import Commitments from '@/components/Commitments'
+import Tasks from '@/components/Tasks/Main'
+import Commitments from '@/components/Commitments/Main'
 
 Vue.use(Router)
 
