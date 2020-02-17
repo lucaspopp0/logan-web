871c0d7c7ce4435961881ba27b31786ba456620c
Lucas Popp <lmp122@case.edu>
2020-02-17 20:07:37 +0100

    Added checkbox colors

--- src/components/Checkbox/script.js
+++ src/components/Checkbox/script.js
@@ -1,7 +1,21 @@
 export default {
     name: 'checkbox',
-    props: ['checked'],
+    props: ['checked', 'color'],
     data() {
         return {}
+    },
+    computed: {
+        styleObject() {
+            let color = !!this.color ? '#' + this.color : 'rgba(0, 0, 0, 0.3)';
+            let style = {
+                border: 'solid 1px rgba(0, 0, 0, 0.3)',
+                background: 'none'
+            };
+
+            if (this.checked)
+                style.background = `radial-gradient(circle at center, ${color} 0%, ${color} 48.5%, rgba(0, 0, 0, 0) 51.5%, rgba(0, 0, 0, 0) 100%)`;
+            
+            return style;
+        }
     }
 }
