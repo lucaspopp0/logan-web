b9c72c882da63b6e9bf66816a2e12bbd4f43a278
Lucas Popp <lmp122@case.edu>
2020-02-20 17:40:53 +0100

    Fixed placeholder properties on task list item

--- src/components/Tasks/TaskListItem/script.js
+++ src/components/Tasks/TaskListItem/script.js
@@ -1,10 +1,24 @@
-import Checkbox from '@/components/Controls/Checkbox'
+import Checkbox from '@/components/Controls/Checkbox';
+import moment from 'moment';
 
 export default {
     name: 'task-list-item',
     props: ['task'],
     data() {
         return {}
+    },
+    computed: {
+        mDueDate() {
+            if (!this.task || (this.task.dueDate == 'e' || this.task.dueDate == 'a'))
+                return null;
+            
+            return moment(this.task.dueDate, 'M/D/YYYY');
+        },
+        overdue() {
+            const d = this.mDueDate;
+            const today = moment(moment().format('MM-DD-YYYY'), 'MM-DD-YY');
+            return d != null && d.isBefore(today);
+        }
     },
     components: { Checkbox }
 }
