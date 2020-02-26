5176a41e2b3f25474a6f39a862e6a7d351258ce6
Lucas Popp <lmp122@case.edu>
2020-02-26 15:14:54 +0100

    Added functionality to create new sections

--- src/components/Commitments/CourseDetailView/script.js
+++ src/components/Commitments/CourseDetailView/script.js
@@ -1,3 +1,6 @@
+import moment from 'moment';
+import api from '@/api';
+import DataManager from '@/data-manager';
 import SectionListItem from '../SectionListItem'
 import SectionDetailView from '../SectionDetailView'
 
@@ -13,6 +16,21 @@ export default {
     methods: {
         select(section) {
             this.currentSelection = section;
+        },
+        newSection() {
+            let newsec = {
+                cid: this.course.cid,
+                name: 'Untitled',
+                weeklyRepeat: 1,
+                start: moment(this.course.semester.startDate).format('MM/DD/YYY HH:mm'),
+                end: moment(this.course.semester.endDate).format('MM/DD/YYY HH:mm'),
+            }
+
+            api.addSection(newsec)
+            .then(response => {
+                this.course.sections.push(response);
+                DataManager.fetchAllData();
+            })
         }
     }
 }
