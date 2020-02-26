fdb8c5b0fffcc89458e37919761681bc72a308ec
Lucas Popp <lmp122@case.edu>
2020-02-26 15:14:34 +0100

    Added priority styling to checkboxes

--- src/components/Controls/Checkbox/script.js
+++ src/components/Controls/Checkbox/script.js
@@ -1,6 +1,19 @@
 export default {
     name: 'checkbox',
-    props: ['checked', 'color'],
+    props: {
+        checked: {
+            type: Boolean,
+            default: false
+        },
+        color: {
+            type: String,
+            default: ''
+        },
+        priority: {
+            type: Number,
+            default: 0
+        }
+    },
     data() {
         return {}
     },
@@ -8,13 +21,38 @@ export default {
         styleObject() {
             let color = !!this.color ? '#' + this.color : 'rgba(0, 0, 0, 0.3)';
             let style = {
-                border: 'solid 1px rgba(0, 0, 0, 0.3)',
+                border: 'solid 1px ' + color,
                 background: 'none'
             };
 
             if (this.checked)
-                style.background = `radial-gradient(circle at center, ${color} 0%, ${color} 48.5%, rgba(0, 0, 0, 0) 51.5%, rgba(0, 0, 0, 0) 100%)`;
+                style.background = `radial-gradient(circle at center, ${color} 0%, ${color} 54.5%, rgba(0, 0, 0, 0) 55.5%, rgba(0, 0, 0, 0) 100%)`;
             
+            return style;
+        },
+        priorityText() {
+            switch (this.priority) {
+                case 2:
+                    return '!!';
+                case 1:
+                    return '!';
+                case 0:
+                    return '';
+                case -1:
+                    return '~';
+                case -2:
+                    return '≈';
+            }
+        },
+        priorityLabelStyle() {
+            const style = {
+                color: '#' + this.color
+            };
+
+            if (this.priority > 0) {
+                style.top = '1px';
+            }
+
             return style;
         }
     }
