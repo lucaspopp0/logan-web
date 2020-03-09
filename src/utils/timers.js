4316a7fa1236ffbd0939dce994c949d70f268ad7
Lucas Popp <lmp122@case.edu>
2020-03-09 17:08:53 +0100

    Fixed UpdateTimer to use intervals instead of timeouts

--- src/utils/timers.js
+++ src/utils/timers.js
@@ -1,59 +1,76 @@
 export class BasicTimer {
 
     constructor(timeout, onComplete) {
-        this.timeout = timeout;
-        this.onComplete = onComplete;
+        this._timeout = timeout;
+        this._onComplete = onComplete;
         this.isOn = false;
-        this.timer = undefined;
+        this._timer = undefined;
     }
 
     // Start the timer. Data passed to completion handler on timeout
     begin(data) {
-        this.timer = setTimeout(() => { 
-            this.execute(data) 
-        }, this.timeout);
+        this._timer = setTimeout(() => { 
+            this._execute(data) 
+        }, this._timeout);
 
         this.isOn = true;
     }
 
     // Internal function. Calls the completion handler
-    execute(data) {
+    _execute(data) {
         this.cancel();
-        this.onComplete(data);
+        this._onComplete(data);
     }
 
     // Stops the timer
     cancel() {
-        if (this.timer != undefined) clearTimeout(this.timer);
+        if (this._timer != undefined) clearTimeout(this._timer);
         this.isOn = false;
-        this.timer = undefined;
+        this._timer = undefined;
     }
 
 }
 
 export class UpdateTimer extends BasicTimer {
 
+    constructor(timeout, onComplete) {
+        super(timeout, onComplete);
+        this._mostRecentData = undefined;
+    }
+
     begin(data) {
-        this.cancel();
-        this.mostRecentData = data;
-        super.begin(data);
+        this._mostRecentData = data;
+
+        this._timer = setInterval(() => {
+            this._execute(data);
+        }, this._timeout);
+
+        this.isOn = true;
+    }
+
+    // Execution function for the timer
+    _execute(data) {
+        this._onComplete(data);
     }
 
     // Calls the completion handler and restarts the timer
     fire(data) {
-        this.mostRecentData = data;
-        this.execute(data);
-    }
-
-    // Calls the completion handler and makes the timer repeat
-    execute(data) {
-        this.onComplete(data);
+        this.cancel();
+        this._execute(data);
         this.begin(data);
     }
 
     // Cancels the current timer and restarts it
     reset() {
-        this.begin(this.mostRecentData);
+        this.cancel();
+        this.begin(this._mostRecentData);
+    }
+
+    cancel() {
+        if (this._timer != undefined) clearInterval(this._timer);
+        this._mostRecentData = undefined;
+        this.isOn = false;
+        this._timer = undefined;
     }
     
 }
