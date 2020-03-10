9b481f2bba61c202874c5a96d985be96736f4e4d
Lucas Popp <lmp122@case.edu>
2020-03-10 15:54:19 +0100

    Added smarter logic to sync button

--- src/components/Main/script.js
+++ src/components/Main/script.js
@@ -1,30 +1,43 @@
 import DataManager from '@/data-manager'
 
 export default {
-  name: 'app',
-  data() { 
-    return {
-      isSignedIn: false
-    } 
-  },
-  mounted() {
-    DataManager.addListener(this);
-    
-    if (process.env.NODE_ENV == 'development') DataManager.signIn();
-  },
-  beforeDestroy() {
-    DataManager.removeListener(this);
-  },
-  methods: {
-    authCallback: function(googleUser) {
-      DataManager.signIn(googleUser);
+    name: 'app',
+    data() { 
+        return {
+            isSignedIn: false,
+            syncing: false
+        } 
     },
-    dmEvent: function(event, data) {
-      if (event == DataManager.EventType.SIGNIN) 
-        this.isSignedIn = DataManager.isSignedIn();
+    mounted() {
+        DataManager.addListener(this);
+        
+        if (process.env.NODE_ENV == 'development')
+            DataManager.signIn();
     },
-    sync() {
-      DataManager.fetchAllData();
+    beforeDestroy() {
+        DataManager.removeListener(this);
+    },
+    methods: {
+        authCallback: function(googleUser) {
+            DataManager.signIn(googleUser);
+        },
+        dmEvent: function(event, data) {
+            switch (event) {
+                case DataManager.EventType.SIGNIN:
+                    this.isSignedIn = DataManager.isSignedIn();
+                    break;
+
+                case DataManager.EventType.FETCH_START:
+                    this.syncing = true;
+                    break;
+
+                case DataManager.EventType.FETCH_COMPLETE:
+                    this.syncing = false;
+                    break;
+            }
+        },
+        sync() {
+            DataManager.fetchAllData();
+        }
     }
-  }
 }
