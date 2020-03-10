a21e6a2ebcac93406c9974beae506cf8b450f3c8
Lucas Popp <lmp122@case.edu>
2020-03-10 15:54:38 +0100

    Fixed null selection bug

--- src/components/Commitments/CourseDetailView/script.js
+++ src/components/Commitments/CourseDetailView/script.js
@@ -24,7 +24,7 @@ export default {
     },
     data() {
         return {
-            currentSelection: null,
+            currentSelection: undefined,
             changesPresent: false,
             timer: undefined
         }
