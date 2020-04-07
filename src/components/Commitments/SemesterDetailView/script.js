ee258549421466433c11474c29eaccadaaf779e4
Lucas Popp <lmp122@case.edu>
2020-04-06 20:17:45 -0700

    Updated SemesterDetailView to use update handler

--- src/components/Commitments/SemesterDetailView/script.js
+++ src/components/Commitments/SemesterDetailView/script.js
@@ -2,10 +2,11 @@ import moment from "moment";
 import api from '@/api';
 import { Semester } from '@/data-types';
 import { PICKER_DATE_FORMAT } from '@/utils/dates';
-import { UpdateTimer } from '@/utils/timers';
+import UpdateHandler from '@/mixins/update-handler';
 
 export default {
     name: 'semester-detail-view',
+    mixins: [UpdateHandler],
     props: {
         semester: {
             type: Semester,
@@ -18,22 +19,17 @@ export default {
             }
         }
     },
-    data() {
-        return {
-            changesPresent: false,
-            timer: undefined
-        }
-    },
     mounted() {
-        this.timer = new UpdateTimer(2000, () => { this.update(this.semester) });
+        this.setupHandlers({
+            update: () => { api.updateSemester(this.semester) },
+            exit: api.updateSemester,
+            change: () => { this.$emit('change', this.semester) },
+            delete: () => { api.deleteSemester(this.semester) }
+        });
     },
     watch: {
         semester(newValue, oldValue) {
-            if (this.changesPresent) {
-                this.update(oldValue);
-            }
-
-            this.timer.cancel();
+            this.propertyChanged(oldValue);
         }
     },
     computed: {
@@ -52,32 +48,6 @@ export default {
             set(newValue) {
                 this.semester.endDate = moment(newValue, PICKER_DATE_FORMAT);
             }
-        }
-    },
-    methods: {
-        registerChange() {
-            console.log('Semester changed');
-            this.changesPresent = true;
-
-            if (this.timer.isOn) this.timer.reset();
-            else this.timer.begin();
-
-            this.$emit('change', this.semester);
-        },
-        update(semesterToUpdate) {
-            console.log('Attempting to update semester');
-            this.timer.cancel();
-
-            if (this.changesPresent) {
-                api.updateSemester(semesterToUpdate);
-            }
-
-            this.changesPresent = false;
-        },
-        deleteSemester() {
-            this.timer.cancel();
-            this.$emit('delete');
-            api.deleteSemester(this.semester);
         }
     }
 }
